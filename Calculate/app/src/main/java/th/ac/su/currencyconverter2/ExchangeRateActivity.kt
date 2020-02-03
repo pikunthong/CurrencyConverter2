5974dae77b201f7493b7d4a9d7a103e43f0fcbb5
pikun <57031274+pikunthong@users.noreply.github.com>
2020-02-03 14:54:28 +0700

    update 3

--- Calculate/app/src/main/java/th/ac/su/currencyconverter2/ExchangeRateActivity.kt
+++ Calculate/app/src/main/java/th/ac/su/currencyconverter2/ExchangeRateActivity.kt
@@ -1,12 +1,32 @@
 package th.ac.su.currencyconverter2
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ExchangeRateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exchange_rate)
+
+        var btnSave = findViewById<Button>(R.id.btnSave)
+        var edtRate = findViewById<EditText>(R.id.edtRate)
+
+        var rate = intent.getDoubleExtra("rate",-1.0)
+
+        edtRate.setText(rate.toString())
+
+        btnSave.setOnClickListener {
+
+            var intent = Intent()
+            intent.putExtra("rate",edtRate.text.toString().toDouble())
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+        }
+
     }
 }
--- Calculate/app/src/main/java/th/ac/su/currencyconverter2/MainActivity.kt
+++ Calculate/app/src/main/java/th/ac/su/currencyconverter2/MainActivity.kt
@@ -1,12 +1,68 @@
 package th.ac.su.currencyconverter2
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    var exchangeRate:Double? = null
+
+    var CHANGE_REQOUEST_CODE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnChangeRate = findViewById<Button>(R.id.btnEdit)
+        var tvRateStatus = findViewById<TextView>(R.id.tvRateStatus)
+
+        var btnConvert = findViewById<Button>(R.id.btnConvert)
+        var edtUsd = findViewById<EditText>(R.id.edtUsd);
+        var tvResult = findViewById<TextView>(R.id.tvResult)
+
+        exchangeRate = 30.0
+
+        btnChangeRate.setOnClickListener {
+
+            var intent = Intent(this@MainActivity,ExchangeRateActivity::class.java)
+            intent.putExtra("rate",exchangeRate!!)
+
+            startActivityForResult(intent,CHANGE_REQOUEST_CODE)
+
+
+
+
+        }
+
+        btnConvert.setOnClickListener {
+
+            var usd:Double = edtUsd.text.toString().toDouble()
+            var thb = usd * exchangeRate!!
+
+            tvResult.setText(thb.toString()+"THB")
+
+
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode==CHANGE_REQOUEST_CODE && resultCode==Activity.RESULT_OK){
+            val rate = data!!.getDoubleExtra("rate",-1.0)
+            exchangeRate = rate
+
+            tvRateStatus.setText("*1.00 USD = "+exchangeRate+"THB")
+        }
+    }
+
+
+
 }
