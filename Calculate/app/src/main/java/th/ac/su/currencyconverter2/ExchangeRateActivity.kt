1f74e5a57db944624961c00aacf460c4426eb0d2
pikun <57031274+pikunthong@users.noreply.github.com>
2020-02-03 13:13:18 +0700

    update 2

---
+++ Calculate/app/src/main/java/th/ac/su/currencyconverter2/ExchangeRateActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.su.currencyconverter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExchangeRateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchange_rate)
+    }
+}
